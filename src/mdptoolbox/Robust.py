dbe9e6b0b3d1e62582034d319d151bf3e1bd117c
Sytze Andringa <s.p.e.andringa@student.tudelft.nl>
2019-05-25 23:33:31 +0200

    dimension analysis for computeSigma, which is not yet correct

--- src/mdptoolbox/Robust.py
+++ src/mdptoolbox/Robust.py
@@ -8,10 +8,12 @@ import numpy as _np
 import scipy.sparse as _sp
 
 
-class RobustPolicyImprovement(MDP):
+class RobustIntervalModel(MDP):
     def __init__(self, transitions, reward, discount, epsilon=0.01,
-                 max_iter=10, skip_check=False):
-        MDP.__init__(self, transitions, reward, discount,epsilon, max_iter, skip_check)
+                 max_iter=10, skip_check=False, P_interval):
+        MDP.__init__(self, transitions, reward, discount, epsilon, max_iter, skip_check)
+        # In the robust interval model, each p is given a lower and upper bound
+        self.P_interval = P_interval
 
 
     def run(self):
@@ -22,6 +24,8 @@ class RobustPolicyImprovement(MDP):
         # TODO perhaps there can be a better initial guess. (v > 0)
         self.v = _np.ones(self.S)
 
+
+
         while True:
             self.iter += 1
 
@@ -31,7 +35,7 @@ class RobustPolicyImprovement(MDP):
             for s in range(self.S):
                 for a in range(self.A):
                     # TODO not sure about dimension of reward
-                    value = self.reward[a, s] +  self.discount * self.sigma[s,a]
+                    value = self.reward[a, s] + self.discount * self.sigma[s,a]
                     if self.v_next[s] > value:
                         self.v_next[s] = value
 
@@ -48,6 +52,32 @@ class RobustPolicyImprovement(MDP):
         self._endRun()
 
     def computeSigma(self):
-        # TODO
+        # TODO: implement this method
+        # n <- amount of states
+        # T <- time horizon
+        #
+        # decision variables :
+        # mu: policy (one action per time horizon, thus t_max amound of actions) T x 1
+        #
+        # hyperparameters :
+        # p_up, p_low: (nonnegative n-vectors, so per state one value) S x 1
+        # v_t(i): (worst case optimal value function in state i at stage t)
+        #
+        # the formula is
+        # sigma = minimize{
+        #   transpose(p_up - p_low) x positive_part(mu x 1-vector - v)
+        #   + transpose(v) x p_up
+        #   + mu (1 - transpose(p_up) x 1-vector)
+        # }
+        ## dimension analysis, todo: get rid of the inconsistencies
+        # sigma has one value per state-action combination: S x A (or A x S ?)
+        # v has a value per state: S x 1
+        # (a) transpose(p_up - p_low) -> 1 x S
+        # (b) positive_part(mu x 1-vector - v) -> T x 1 * S x 1 = ??? but probably S x T (not sure how mu * 1-vector works)
+        # (a) x (b) -> 1 x S * S * T -> 1 x T
+        # transpose(v) x p_up -> 1 x S * S x 1 -> 1 x 1
+        # mu (1 - transpose(p_up) x 1-vector) ->  T x 1 * (1 - 1 x S * S x 1) = T x 1 * 1 x 1 = T x 1
+        # sigma = 1 x T + 1 x 1 + 1 x T = 1 x T <- (not S x A or A x S)
+        
         self.sigma = self.v.copt()
         pass
