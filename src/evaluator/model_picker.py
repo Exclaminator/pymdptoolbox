f06e0d9c9996135319c2ca80818ba6d1136b59c4
Sytze Andringa <s.p.e.andringa@student.tudelft.nl>
2019-06-12 01:38:29 +0200

    removed some lines in model_picker to show its intented behavior

--- src/evaluator/model_picker.py
+++ src/evaluator/model_picker.py
@@ -36,17 +36,10 @@ def create_mdp(mdp_as_dict, problem):
     elif mdp_type == ROBUST_MDP_KEY:
         #####
         # todo: let this blend with youri's changes for the robust MDP model
-
-        sigma_identifier = mdp_as_dict[SIGMA_IDENTIFIER_KEY]
-
-
+        sigma_function = _get_sigma_function(mdp_as_dict[SIGMA_IDENTIFIER_KEY])
         mdp_out = mdptoolbox.Robust.RobustModel(
-            transition_kernel, reward_matrix, discount=discount_factor,
-            p_lower=interval["p_low"], p_upper=interval["p_up"],
-            sigma_identifier=retrieve_from_dict(mdp_hyperparameters, "sigma_identifier", "interval")
+            transition_kernel, reward_matrix, discount=discount_factor, sigma_function
         )
-        if mdp_hyperparameters["sigma_identifier"] == "ellipsoid":
-            mdp_out.max_iter = 10
         #####
 
     elif mdp_type == VALUE_MDP_KEY:
@@ -62,6 +55,4 @@ def _get_sigma_function(identifier):
     # todo: implement based on youri's implementation
     if identifier == INTERVAL_KEY:
 
-
-
-    return
+    return -1
