e3741573016dd830f9a34bf983e80a61322f3ce2
Jens Langerak <j.langerak@student.tudelft.nl>
2019-06-11 17:37:58 +0200

    For plots select by default latest logs

--- src/scripts/plots.py
+++ src/scripts/plots.py
@@ -1,15 +1,25 @@
 import matplotlib.pyplot as plt
 import csv
+import os
 import numpy as np
 
 # DEBUG instructions: Unexpected results? Check the following things:
 # Is the row filter correct?
-# Is the folder set?
+# Is the folder correct?
 
 # settings
-folder = "20190611-170535"
+
+#select folder of the logs, if None then latest folder is used
+folder = None #"20190611-170535"
+logpath = '..\\..\\logs\\'
+if folder is None:
+    dirs = [d for d in os.listdir(logpath) if os.path.isdir(logpath + d)]
+    sorted = sorted(dirs, key=lambda x: os.path.getctime(logpath + x), reverse=True)
+    folder = sorted[0]
+
 
 #only select rows with the given value for the specified columns (usefull when you have changed multiple values
+#usage specify for each fixed column its value. If column is not added to row_filte, any value is accepted
 row_filter = {
     'beta':'0.2',
     'delta':'0.2'
@@ -24,7 +34,7 @@ labelColumn = 'mdp_id'
 results = {}
 x = []
 
-with open('..\\..\\logs\\' + folder + '\\results_params.log', mode='r') as csv_file:
+with open(logpath + folder + '\\results_params.log', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
         # check if row should be read
