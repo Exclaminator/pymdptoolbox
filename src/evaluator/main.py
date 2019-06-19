87cafd47e6f34accd508b0f1e80e0662aea1efdd
Youri Arkesteijn <youri.essa@gmail.com>
2019-06-19 16:01:12 +0200

    Problems and MDP's are now a list

--- src/evaluator/main.py
+++ src/evaluator/main.py
@@ -1,12 +1,7 @@
-import Evaluator
+from Evaluator import Evaluator
 from Options import Options
-from mdptoolbox.Robust import Robust
+from mdptoolbox.Robust import *
 from ProblemSet import normalize_tk
-
-from mdptoolbox.InnerMethod.Wasserstein import Wasserstein
-from mdptoolbox.InnerMethod.Ellipsoid import Ellipsoid
-from mdptoolbox.InnerMethod.Likelihood import Likelihood
-from mdptoolbox.InnerMethod.Interval import Interval
 
 from mdptoolbox.mdp import ValueIteration
 
@@ -19,34 +14,33 @@ default configuration, runs the forest problem on some models
 
 
 def run_default():
-
     # get tk_low and tk_up for the interval model
-    forestProblem = Problem.create_forest_problem(S=10, discount_factor=0.9, r1=10, r2=2, p=0.05)
-    tk = forestProblem.transition_kernel
+    forest = Problem.get_forest_problem(S=10, discount_factor=0.9, r1=10, r2=2, p=0.05)
+    tk = forest.transition_kernel
     tk_low = (tk-0.7).clip(min=0)
     tk_up = (tk+0.4).clip(max=1)
 
-    options = Options(
-        number_of_paths=100,
-        number_of_sims=1000,
-        plot_hist=True,
-        do_simulation=False,
-        evaluate_all=True,
-        evaluate_inner=True,
-        sample_var=0.15,
-        sample_amount=1000,
-    )
-    problem_dict = {
-        "forest": forestProblem
-    }
-    mdp_dict = {
-        "wasserstein-0.1": Robust(Wasserstein(0.1)),
-        "ellipsoid-0.12": Robust(Ellipsoid(0.12)),
-        "value_iteration": ValueIteration,
-        "max_likelihood-0.2-0.1": Robust(Likelihood(0.2, 0.1)),
-        "interval": Robust(Interval(tk_low, tk_up))
-    }
-    Evaluator.build_and_run(problem_dict, mdp_dict, options)
+    # problems can also be supplied as a list
+    evaluator = Evaluator(
+        forest,
+        [
+            Robust(Wasserstein(0.1)),
+            Robust(Ellipsoid(0.12)),
+            ValueIteration,
+            Robust(Likelihood(0.2, 0.1)),
+            Robust(Interval(tk_low, tk_up))
+        ],
+        Options(
+            number_of_paths=100,
+            number_of_sims=1000,
+            plot_hist=True,
+            do_simulation=False,
+            evaluate_all=True,
+            evaluate_inner=True,
+            sample_var=0.15,
+            sample_amount=1000,
+        ))
+    evaluator.run()
 
 
 run_default()
