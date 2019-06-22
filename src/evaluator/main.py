7984c6c3ea4df3c668fee88678a182bdfb315e99
Jens Langerak <j.langerak@student.tudelft.nl>
2019-06-23 01:46:17 +0200

    Some testing with random problems

--- src/evaluator/main.py
+++ src/evaluator/main.py
@@ -13,8 +13,9 @@ default configuration, runs the forest problem on some models
 
 def run_default():
     # get tk_low and tk_up for the interval model
-    forest = Problem.get_forest_problem(S=10, discount_factor=0.9, r1=10, r2=2, p=0.05)
-    tk_low, tk_up = Interval.compute_interval(forest.transition_kernel, 0.0138)
+   # forest = Problem.get_forest_problem(S=10, discount_factor=0.9, r1=10, r2=2, p=0.05)
+    forest = Problem.get_random_problem()
+    tk_low, tk_up = Interval.compute_interval(forest.transition_kernel, 0.08)
     # tk_low = (tk-0.5).clip(min=0)
     # tk_up = (tk+0.5).clip(max=1)
 
@@ -22,14 +23,14 @@ def run_default():
     evaluator = Evaluator(
         forest,
         [
-            Robust(Wasserstein(0.07)),
-            Robust(Ellipsoid(0.1312)),
+            Robust(Wasserstein(0.16)),
+            Robust(Ellipsoid(0.098)),
             ValueIteration,
-            Robust(Likelihood(0.6125, 0.001)), #range 1.5 - 0ish
+            Robust(Likelihood(4.1, 0.001)), #range 1.5 - 0ish
             Robust(Interval(tk_low, tk_up))
         ],
         Options(
-            number_of_paths=2000,
+            number_of_paths=1000, # set size of in and out set
             number_of_sims=1000,
             plot_hist=True,
             do_simulation=False,
@@ -37,7 +38,7 @@ def run_default():
             evaluate_inner=True,
             evaluate_outer=True,
             sample_var=0.5,
-            sample_amount=2000,
+            sample_amount=3000, # is used for all_samples
             sample_uniform=False,
             non_robust_actions=[], # replace with [1] if for action there should be no robustness
             variance_scaling=True,
